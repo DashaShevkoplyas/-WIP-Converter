39b1057c0031c1bc1c4b97765fec986d4c15cd08
DashaShevkoplyas <shevkoplyasdarya23@gmail.com>
2023-09-07 22:08:31 +0300

    Move methods to separate class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,20 @@
+import cloudconverter.UploadToCloudConverter;
 import com.cloudconvert.client.CloudConvertClient;
-import com.cloudconvert.client.setttings.StringSettingsProvider;
-import com.cloudconvert.exception.CloudConvertClientException;
-import com.cloudconvert.exception.CloudConvertServerException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uploader.FileUploader;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class Main {
-    public static void main(String[] args) throws IOException, CloudConvertServerException, CloudConvertClientException, URISyntaxException {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
         FileUploader processor = new FileUploader();
         var file = processor.uploadLastCreatedFile();
-        System.out.println("The last file that was created: " + file.getName());
-        final CloudConvertClient cloudConvertClient =
-                new CloudConvertClient(new StringSettingsProvider("api-key", "webhook-signing-secret", false));
-        System.out.println(cloudConvertClient.tasks().list());
+        LOGGER.info(String.format("The last file that was created '%s' and stored in the location %s ", file.getName(), file.getParentFile()));
+        var uploadToCloudConverter = new UploadToCloudConverter(new CloudConvertClient());
+        uploadToCloudConverter.uploadLocalFileToConverter(file);
     }
 }
