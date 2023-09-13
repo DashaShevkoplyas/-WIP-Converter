4cf9cf126a311e5d207302819ad2965533ff1deb
DariaShevkoplias <34021206+DashaShevkoplyas@users.noreply.github.com>
2023-09-13 21:58:19 +0300

    Feature/bring changes to separate class (#2)

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,13 @@
+package utils;
+
+/**
+ * Base constants class.
+ */
+public final class Constants {
+    public static final String FORMAT = "mp4";
+    public static final String SYSTEM_PROPERTY = "user.home";
+    public static final String CREATION_TIME_PROP = "creationTime";
+
+    private Constants() {
+    }
+}
